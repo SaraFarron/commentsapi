2a6699574699856a2bf6df0ce6a56a0acec24392
Nikita Syulin <nikita_sulin@idpowers.com>
2021-08-04 19:57:17 +0700

    redo

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+
+
+WORKDIR /code
+COPY . .
+
+
+RUN pip install -U pip && \
+   pip install poetry && \
+   poetry config virtualenvs.create false && \
+   poetry install --no-dev
--- server/server/settings.py
+++ server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%h21yjx7h+1v2e54al2g84516=p3s!^zhyjrx0j)s8*_11g+v^'
+SECRET_KEY = 'django-insecure-2qq1w(r-fk_oa_x+#!v9+0-6_!5m45ib77bn)^lu4xw$5ua4e('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
